ca91a65fd7ee7f1092b417e97bca627e551484ee
Gavrilita Rolland <gavrilita_rolland@yahoo.com>
2017-05-14 00:50:36 +0300

    Update LocationServiceTest.java

--- src/test/java/com/hazardmanager/users/services/LocationServiceTest.java
+++ src/test/java/com/hazardmanager/users/services/LocationServiceTest.java
@@ -1,14 +1,44 @@
 package com.hazardmanager.users.services;
 
+import javax.tools.JavaFileManager.Location;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 
-//public class LocationServiceTest {
-//
-//
-//
-//    @Test
-//    public void when_called_with{
-//
-//    }
-//}
+
+public class LocationServiceTest {
+
+      private Location location;
+
+    @Before
+    public void buildUp() {
+        location = new Location("590b1c84ce65c6189c9ca4ff") {
+            public String getName() {
+                throw new UnsupportedOperationException("Not supported yet."); 
+            }
+
+            public boolean isOutputLocation() {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+        };
+    }
+
+    @After
+    public void tearDown() {
+        location = null;
+    }
+    
+    @Test
+    public void getAllUserLocationsTest {
+        location.setAlias(alias);
+        Assert.assertEquals(location.getAllUserLocations("userid") , null);
+   }
+    @Test
+     public void getLocationByUserIdAndAliasTest()
+     {
+        Assert.assertEquals(location.getLocationByUserIdAndAlias("user","alias") , null);
+     }
+     
+
+}
